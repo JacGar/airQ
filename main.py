8b9a2fbb00c53b47e3223fb728a672176200401a
Jacinto Garcia <jacintogm84@gmail.com>
2018-02-27 15:13:54 +0100

    Test for downloading all files, before implementing multithreading

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 from urllib.request import urlretrieve
 import pandas as pd
 import os
+import time
 
+start = time.time()
 url = 'http://data.aireas.com/csv/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html5lib')
@@ -15,6 +17,7 @@ content = table.find_all('tr')
 folders = list()
 files = list()
 
+print('Getting files info')
 for item in content:
     try:
         if 'DIR' in item.find('img').get('alt'):
@@ -24,28 +27,32 @@ for item in content:
     except AttributeError:
         pass
 
-file = urlretrieve(url+files[-1])
+for item in files:
+    file_start = time.time()
+    print('Parsing file: {}'.format(item), end='\t')
+    file = urlretrieve(url+item)
 
-with open(file[0]) as data:
-    counter = 0
-    for line in data:
-        if 'UNITS' in line:
-            unit_row = counter
-        if 'LOCATIONS' in line:
-            loc_row = counter
-        if 'MEASUREMENTS' in line:
-            meas_row = counter
-        counter += 1
+    with open(file[0]) as data:
+        counter = 0
+        for line in data:
+            if 'UNITS' in line:
+                unit_row = counter
+            if 'LOCATIONS' in line:
+                loc_row = counter
+            if 'MEASUREMENTS' in line:
+                meas_row = counter
+            counter += 1
+
+        data.seek(0)
+        units = pd.read_csv(data, sep=';', skiprows=[unit_row] + [x for x in range(loc_row, counter)])
+        data.seek(0)
+        locations = pd.read_csv(data, sep=';', skiprows=[x for x in range(loc_row + 1)] + [x for x in range(meas_row, counter)])
+        data.seek(0)
+        measurements = pd.read_csv(data, sep=';', skiprows=[x for x in range(meas_row + 1)])
+    os.remove(file[0])
+    print('{:1.2f} seconds'.format(time.time() - file_start))
+
+print('Total execution time for {} files: {} seconds'.format(len(files), time.time() - start))
 
 
-    print(unit_row, loc_row, meas_row, counter)
-    data.seek(0)
-    units = pd.read_csv(data, sep=';', skiprows=[unit_row] + [x for x in range(loc_row, counter)])
-    data.seek(0)
-    locations = pd.read_csv(data, sep=';', skiprows=[x for x in range(loc_row + 1)] + [x for x in range(meas_row, counter)])
-    data.seek(0)
-    measurements = pd.read_csv(data, sep=';', skiprows=[x for x in range(meas_row + 1)])
-os.remove(file[0])
-
-print(measurements)
 
