749327c33535c5ee8551dbf200d6b7ddf6cdb148
Jacinto Garcia <jacintogm84@gmail.com>
2018-02-27 16:29:32 +0100

    Playing around with multithreading (8x time improvement)

--- main.py
+++ main.py
@@ -4,36 +4,18 @@ from urllib.request import urlretrieve
 import pandas as pd
 import os
 import time
+from queue import Queue
+from threading import Thread
+import logging
 
-start = time.time()
-url = 'http://data.aireas.com/csv/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html5lib')
 
-table = soup.find('table')
-
-content = table.find_all('tr')
-
-folders = list()
-files = list()
-
-print('Getting files info')
-for item in content:
-    try:
-        if 'DIR' in item.find('img').get('alt'):
-            folders.append(item.find('a').get('href'))
-        elif 'TXT' in item.find('img').get('alt'):
-            files.append(item.find('a').get('href'))
-    except AttributeError:
-        pass
-
-for item in files:
-    file_start = time.time()
-    print('Parsing file: {}'.format(item), end='\t')
-    file = urlretrieve(url+item)
-
+def download_link(directory, link):
+    start = time.time()
+    logging.info('Downloading {}'.format(link))
+    file = urlretrieve(directory + link)
     with open(file[0]) as data:
         counter = 0
+        unit_row, loc_row,meas_row = 0,0,0
         for line in data:
             if 'UNITS' in line:
                 unit_row = counter
@@ -42,17 +24,65 @@ for item in files:
             if 'MEASUREMENTS' in line:
                 meas_row = counter
             counter += 1
-
         data.seek(0)
         units = pd.read_csv(data, sep=';', skiprows=[unit_row] + [x for x in range(loc_row, counter)])
         data.seek(0)
-        locations = pd.read_csv(data, sep=';', skiprows=[x for x in range(loc_row + 1)] + [x for x in range(meas_row, counter)])
+        locations = pd.read_csv(data, sep=';',
+                                skiprows=[x for x in range(loc_row + 1)] + [x for x in range(meas_row, counter)])
         data.seek(0)
         measurements = pd.read_csv(data, sep=';', skiprows=[x for x in range(meas_row + 1)])
     os.remove(file[0])
-    print('{:1.2f} seconds'.format(time.time() - file_start))
-
-print('Total execution time for {} files: {} seconds'.format(len(files), time.time() - start))
-
+    logging.info('{} downloaded in {:1.2f} seconds'.format(link, time.time() - start))
+    return units, locations, measurements
 
 
+class FileHandlerWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            directory, link = self.queue.get()
+            download_link(directory, link)
+            self.queue.task_done()
+
+
+def main():
+    logging.basicConfig(filename='log.log', level=logging.DEBUG)
+    start = time.time()
+    url = 'http://data.aireas.com/csv/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html5lib')
+    table = soup.find('table')
+    content = table.find_all('tr')
+    folders = list()
+    files = list()
+
+    # Create list of files
+    logging.info('Getting list of files to download')
+    for item in content:
+        try:
+            if 'DIR' in item.find('img').get('alt'):
+                folders.append(item.find('a').get('href'))
+            elif 'TXT' in item.find('img').get('alt'):
+                files.append(item.find('a').get('href'))
+        except AttributeError:
+            pass
+
+    queue = Queue()
+    # Create workers
+    for x in range(8):
+        worker = FileHandlerWorker(queue)
+        worker.daemon = True
+        worker.start()
+    # Fill queue with files to parse:
+    for file in files:
+        logging.info('Queueing {}'.format(file))
+        queue.put((url, file))
+    queue.join()
+    logging.info('Total execution time for {} files: {} seconds'.format(len(files), time.time() - start))
+
+
+if __name__ == "__main__":
+    main()
